bba5bc9236b873611e5a256ba80ce2b6b8693135
Patrick Reynolds <patrick.reynolds@affirm.com>
2019-01-25 14:09:31 -0800

    Temp version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer"
+import Temp from "./Temp"
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.temp = true
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to update.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.temp) {
+      return (
+        <div className="app">
+          <Temp></Temp>
+        </div>)
+      } else {
+        return (
+        <HashRouter>
+          <div className="app">
+            <header className="header">
+              <NavLink exact to="/" className="header-name" style={{ textDecoration: 'none' }}><h1>Patrick Reynolds</h1></NavLink>
+              <ul className="header-nav">
+                <NavLink to="/about" style={{ textDecoration: 'none' }}><li className="header-nav-item">About</li></NavLink>
+                <NavLink to="/contact" style={{ textDecoration: 'none' }}><li className="header-nav-item">Contact</li></NavLink>
+              </ul>
+            </header>
+            <div className="content">
+              <Route exact path="/" component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route path="/contact" component={Contact}/>
+            </div>
+            <Footer></Footer>
+          </div>
+        </HashRouter>
+        )
+      }
   }
 }
 
---
+++ src/Temp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Temp extends Component {
+  render() {
+    return (
+      <div>
+        <p class="temp">New portfolio coming soon 🤗</p>
+      </div>
+    );
+  }
+}
+ 
+export default Temp;
